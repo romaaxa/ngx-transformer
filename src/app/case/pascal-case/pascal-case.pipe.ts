d449fefae2096a65f6b1537fda71d9fd2d0b6ab9
Anton <a.romashko.work@mail.ru>
2023-02-25 18:42:53 +0300

    [Hotfix] Fix strict null checks

--- src/app/case/pascal-case/pascal-case.pipe.ts
+++ src/app/case/pascal-case/pascal-case.pipe.ts
@@ -3,10 +3,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'pascalCase' })
 export class PascalCasePipe implements PipeTransform {
   transform(value: string): string {
+    if (typeof value !== 'string') {
+      throw new Error('Value is not a string.');
+    }
     return value.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, (match) => {
-      if (typeof value !== 'string') {
-        throw new Error('Value is not a string.');
-      }
       return +match !== 0 ? match.toUpperCase() : '';
     });
   }
